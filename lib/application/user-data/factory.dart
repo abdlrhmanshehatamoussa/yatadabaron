05afdda6885933827dc7a56a1e79dae52c490360
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2021-08-09 23:46:23 +0200

    Fixing bookmarks service bug

--- lib/application/user-data/factory.dart
+++ lib/application/user-data/factory.dart
@@ -1,11 +1,13 @@
 import 'package:Yatadabaron/modules/persistence.module.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'interface.dart';
 import 'user-data-service.dart';
 
 class UserDataServiceFactory {
   static Future<IUserDataService> create() async {
-    await UserDataRepository.instance.initialize();
-    return new UserDataService();
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    UserDataRepository repo = UserDataRepository(pref);
+    return new UserDataService(repo);
   }
 }
--- lib/application/user-data/user-data-service.dart
+++ lib/application/user-data/user-data-service.dart
@@ -3,19 +3,23 @@ import 'package:Yatadabaron/modules/persistence.module.dart';
 import 'interface.dart';
 
 class UserDataService implements IUserDataService {
+  final UserDataRepository _repo;
+
+  UserDataService(this._repo);
+  
   Future<int?> getBookmarkChapter() async {
-    await UserDataRepository.instance.getBookmarkChapter();
+    return await _repo.getBookmarkChapter();
   }
 
   Future<int?> getBookmarkVerse() async {
-    await UserDataRepository.instance.getBookmarkVerse();
+    return await _repo.getBookmarkVerse();
   }
 
   Future<void> setBookmarkVerse(int verseId) async {
-    await UserDataRepository.instance.setBookmarkVerse(verseId);
+    await _repo.setBookmarkVerse(verseId);
   }
 
   Future<void> setBookmarkChapter(int chapterId) async {
-    await UserDataRepository.instance.setBookmarkChapter(chapterId);
+    await _repo.setBookmarkChapter(chapterId);
   }
 }
