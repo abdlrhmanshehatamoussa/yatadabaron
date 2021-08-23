754cbb282a57296caa845021508e055d65b58f64
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2021-08-23 02:21:42 +0200

    Adding build number to the ReleaseInfo dto

--- lib/presentation/pages/release_notes/dtos/release_info.dart
+++ lib/presentation/pages/release_notes/dtos/release_info.dart
@@ -1,9 +1,11 @@
 class ReleaseInfo {
   final String name;
   final String description;
+  final int buildNumber;
 
   ReleaseInfo({
     required this.name,
     required this.description,
+    required this.buildNumber,
   });
 }
