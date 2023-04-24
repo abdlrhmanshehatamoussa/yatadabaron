7bef9ef35b2bb0d602b19a9ff0bbb359a3f5b3f0
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2023-04-24 14:07:19 +0200

    replacing audio packagre

--- lib/pages/tafseer/widgets/play_button.dart
+++ lib/pages/tafseer/widgets/play_button.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
 
 class SingleVersePlayButton extends StatefulWidget {
   final int verseId;
@@ -17,30 +18,53 @@ class SingleVersePlayButton extends StatefulWidget {
 
 class _SingleVersePlayButtonState extends State<SingleVersePlayButton> {
   final AudioPlayer player = AudioPlayer();
+  StreamSubscription<PlayerState>? streamSubscription;
+  PlayerState state = PlayerState.stopped;
+
+  @override
+  void initState() {
+    super.initState();
+    streamSubscription = player.onPlayerStateChanged.listen((playerState) {
+      setState(() {
+        state = playerState;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: () async {
-        var chapterIndex = widget.chapterId.toString().padLeft(3, "0");
-        var verseIndex = widget.verseId.toString().padLeft(3, "0");
-        var reciterName = "Husary_128kbps";
-        await player.setAudioSource(
-          AudioSource.uri(
-            Uri.parse(
-              "https://everyayah.com/data/$reciterName/$chapterIndex$verseIndex.mp3",
-            ),
-          ),
-        );
-        await player.play();
+        switch (state) {
+          case PlayerState.completed:
+          case PlayerState.stopped:
+            var chapterIndex = widget.chapterId.toString().padLeft(3, "0");
+            var verseIndex = widget.verseId.toString().padLeft(3, "0");
+            var reciterName = "Husary_128kbps";
+            await player.play(
+              UrlSource(
+                "https://everyayah.com/data/$reciterName/$chapterIndex$verseIndex.mp3",
+              ),
+            );
+            break;
+          case PlayerState.playing:
+            await player.pause();
+            break;
+          case PlayerState.paused:
+            await player.resume();
+            break;
+        }
       },
-      icon: Icon(Icons.play_arrow),
+      icon: (state == PlayerState.playing)
+          ? Icon(Icons.pause)
+          : Icon(Icons.play_arrow),
     );
   }
 
   @override
   void dispose() async {
-    await player.dispose();
     super.dispose();
+    await streamSubscription?.cancel();
+    await player.dispose();
   }
 }
