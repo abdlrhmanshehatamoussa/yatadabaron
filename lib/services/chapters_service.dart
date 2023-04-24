fd9af3477b9b0108f06eaf55f9460c68cc84b081
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2023-04-24 14:39:51 +0200

    feat: replacing fake chapter service for web

--- lib/services/chapters_service.dart
+++ lib/services/chapters_service.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
 import 'package:yatadabaron/_modules/models.module.dart';
 import 'package:yatadabaron/_modules/service_contracts.module.dart';
 import 'package:yatadabaron/commons/database_mixin.dart';
@@ -59,18 +62,33 @@ class ChaptersService with DatabaseMixin implements IChaptersService {
 class ChaptersServiceWeb extends IChaptersService {
   @override
   Future<List<Chapter>> getAll() async {
-    return [
-      Chapter(chapterID: 1, chapterNameAR: "Test", chapterNameEN: "Test", location: ChapterLocation.MADANI, sajdaLocation: 0, verseCount: 1)
-    ];
+    var response = await get(Uri.parse("http://api.alquran.cloud/v1/meta"));
+    var body = jsonDecode(response.body);
+    var result = <Chapter>[];
+    for (var element in body["data"]["surahs"]["references"]) {
+      result.add(Chapter(
+        chapterID: element["number"],
+        chapterNameAR: element["name"],
+        chapterNameEN: element["englishName"],
+        location: element["revelationType"] == "Meccan"
+            ? ChapterLocation.MAKKI
+            : ChapterLocation.MADANI,
+        sajdaLocation: 0,
+        verseCount: element["numberOfAyahs"],
+      ));
+    }
+    return result;
   }
 
   @override
   Future<Chapter> getChapter(int chapterID) async {
-    return Chapter(chapterID: 1, chapterNameAR: "Test", chapterNameEN: "Test", location: ChapterLocation.MADANI, sajdaLocation: 0, verseCount: 1);
+    var all = await getAll();
+    return all.firstWhere((c) => c.chapterID == chapterID);
   }
 
   @override
   Future<String> getChapterName(int chapterID) async {
-    return "Test";
+    var all = await getAll();
+    return all.firstWhere((c) => c.chapterID == chapterID).chapterNameAR;
   }
 }
