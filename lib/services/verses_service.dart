97a21dda9a3f5ff85dfbd3e5590ce16a77fada8e
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2023-04-24 15:11:15 +0200

    replacing fake verse service

--- lib/services/verses_service.dart
+++ lib/services/verses_service.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
 import 'package:yatadabaron/_modules/models.module.dart';
 import 'package:yatadabaron/_modules/service_contracts.module.dart';
 import 'package:yatadabaron/commons/utils.dart';
@@ -278,24 +281,34 @@ class VerseServiceWeb extends IVersesService {
 
   @override
   Future<Verse> getSingleVerse(int verseId, int chapterId) async {
+    var response = await get(Uri.parse(
+        "https://api.alquran.cloud/v1/ayah/$chapterId:$verseId/quran-usmani"));
+    var body = jsonDecode(response.body);
     return Verse(
       chapterId: chapterId,
       verseID: verseId,
-      verseText: "Testing . ..",
-      verseTextTashkel: "Test test test",
+      verseText: body["data"]["text"],
+      verseTextTashkel: body["data"]["text"],
+      chapterName: body["data"]["surah"]["name"]
     );
   }
 
   @override
   Future<List<Verse>> getVersesByChapterId(int? chapterId, bool basmala) async {
-    return [
-      Verse(
+    var url = "http://api.alquran.cloud/v1/surah/$chapterId/quran-usmani";
+    var response = await get(Uri.parse(url));
+    var body = jsonDecode(response.body);
+    var result = <Verse>[];
+    for (var element in body["data"]["ayahs"]) {
+      result.add(Verse(
         chapterId: chapterId ?? 0,
-        verseID: 1,
-        verseText: "Testing . ..",
-        verseTextTashkel: "Test test test",
-      )
-    ];
+        verseID: element["numberInSurah"],
+        verseText: element["text"],
+        verseTextTashkel: element["text"],
+        chapterName: body["data"]["name"],
+      ));
+    }
+    return result;
   }
 
   @override
