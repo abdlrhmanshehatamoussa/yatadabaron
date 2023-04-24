ff50d7741e0eff4b2bca770b6fdb6d10014836d6
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2023-04-24 17:21:22 +0200

    implementing search module in web platform

--- lib/services/web/verse_service_web.dart
+++ lib/services/web/verse_service_web.dart
@@ -4,6 +4,10 @@ import 'package:yatadabaron/_modules/models.module.dart';
 import 'package:yatadabaron/_modules/service_contracts.module.dart';
 
 class VerseServiceWeb extends IVersesService {
+  final String uthmaniEdition = "quran-uthmani";
+  final String cleanEdition = "quran-simple-clean";
+  String get editionsCommaSeparated => "$uthmaniEdition,$cleanEdition";
+
   @override
   Future<List<LetterFrequency>> getLetterFrequency(
       BasicSearchSettings settings) async {
@@ -13,29 +17,43 @@ class VerseServiceWeb extends IVersesService {
   @override
   Future<Verse> getSingleVerse(int verseId, int chapterId) async {
     var response = await get(Uri.parse(
-        "https://api.alquran.cloud/v1/ayah/$chapterId:$verseId/quran-usmani"));
-    var body = jsonDecode(response.body);
+        "https://api.alquran.cloud/v1/ayah/$chapterId:$verseId/editions/$editionsCommaSeparated"));
+    List<dynamic> editions = jsonDecode(response.body)["data"];
+
+    var noTashkeelEdition = editions.firstWhere(
+        (element) => element["edition"]["identifier"] == cleanEdition);
+
+    var tashkeelEdition = editions.firstWhere(
+        (element) => element["edition"]["identifier"] == uthmaniEdition);
     return Verse(
-        chapterId: chapterId,
-        verseID: verseId,
-        verseText: body["data"]["text"],
-        verseTextTashkel: body["data"]["text"],
-        chapterName: body["data"]["surah"]["name"]);
+      chapterId: chapterId,
+      verseID: verseId,
+      verseText: noTashkeelEdition["text"],
+      verseTextTashkel: tashkeelEdition["text"],
+      chapterName: tashkeelEdition["surah"]["name"],
+    );
   }
 
   @override
   Future<List<Verse>> getVersesByChapterId(int? chapterId, bool basmala) async {
-    var url = "http://api.alquran.cloud/v1/surah/$chapterId/quran-usmani";
+    var url =
+        "http://api.alquran.cloud/v1/surah/$chapterId/editions/$editionsCommaSeparated";
     var response = await get(Uri.parse(url));
-    var body = jsonDecode(response.body);
+    List<dynamic> editions = jsonDecode(response.body)["data"];
+    var noTashkeelEdition = editions.firstWhere(
+        (element) => element["edition"]["identifier"] == cleanEdition);
+    var tashkeelEdition = editions.firstWhere(
+        (element) => element["edition"]["identifier"] == uthmaniEdition);
     var result = <Verse>[];
-    for (var element in body["data"]["ayahs"]) {
+    for (var i = 0; i < tashkeelEdition["ayahs"].length; i++) {
+      var tashkeelAyah = tashkeelEdition["ayahs"][i];
+      var noTashkeelAyah = noTashkeelEdition["ayahs"][i];
       result.add(Verse(
         chapterId: chapterId ?? 0,
-        verseID: element["numberInSurah"],
-        verseText: element["text"],
-        verseTextTashkel: element["text"],
-        chapterName: body["data"]["name"],
+        verseID: tashkeelAyah["numberInSurah"],
+        verseText: noTashkeelAyah["text"],
+        verseTextTashkel: tashkeelAyah["text"],
+        chapterName: tashkeelEdition["name"],
       ));
     }
     return result;
@@ -43,6 +61,57 @@ class VerseServiceWeb extends IVersesService {
 
   @override
   Future<SearchResult> keywordSearch(KeywordSearchSettings settings) async {
+    if (settings.chapterID != null) {
+      var verses =
+          await getVersesByChapterId(settings.chapterID, settings.basmala);
+      switch (settings.mode) {
+        case SearchMode.START:
+          verses = verses
+              .where(
+                (v) => v.verseText.startsWith(settings.keyword),
+              )
+              .toList();
+          break;
+        case SearchMode.END:
+          verses = verses
+              .where(
+                (v) => v.verseText.endsWith(settings.keyword),
+              )
+              .toList();
+          break;
+        case SearchMode.WORD:
+          verses = verses
+              .where(
+                (v) => v.verseText.contains(" ${settings.keyword} "),
+              )
+              .toList();
+          break;
+        case SearchMode.WITHIN:
+          verses = verses
+              .where(
+                (v) => v.verseText.contains(settings.keyword),
+              )
+              .toList();
+          break;
+      }
+      return SearchResult(
+          settings: settings,
+          results: verses
+              .map(
+                (v) => VerseSearchResult(
+                  verse: v,
+                  slices: [
+                    SearchSlice(
+                      start: 0,
+                      end: v.verseText.length,
+                      text: v.verseText,
+                      match: true,
+                    )
+                  ],
+                ),
+              )
+              .toList());
+    }
     return SearchResult(settings: settings, results: []);
   }
 }
