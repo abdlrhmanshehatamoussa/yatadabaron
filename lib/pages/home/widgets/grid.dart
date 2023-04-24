5e366ab75cac516fd232012a3f15c458dbef3195
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2023-04-24 17:42:30 +0200

    finalized

--- lib/pages/home/widgets/grid.dart
+++ lib/pages/home/widgets/grid.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../viewmodels/home_grid_item.dart';
 import 'gird_item.dart';
@@ -8,13 +9,36 @@ class HomeGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> gridItems = items
-        .map((HomeGridItemViewModel item) => HomeGridItem(
-              icon: item.icon,
-              onTap: item.onTap,
-              title: item.title,
-            ))
-        .toList();
+    Widget body;
+    if (kIsWeb) {
+      body = ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          var item = items[index];
+          return Card(
+            child: ListTile(
+              title: Text(item.title),
+              trailing: Icon(item.icon, color: Theme.of(context).colorScheme.secondary,),
+              onTap: () async => await item.onTap(),
+            ),
+          );
+        },
+      );
+    } else {
+      body = GridView.count(
+        crossAxisCount: 3,
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 15,
+        children: items
+            .map((HomeGridItemViewModel item) => HomeGridItem(
+                  icon: item.icon,
+                  onTap: item.onTap,
+                  title: item.title,
+                ))
+            .toList(),
+      );
+    }
+
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.only(
@@ -24,12 +48,7 @@ class HomeGrid extends StatelessWidget {
         color: Theme.of(context).secondaryHeaderColor,
       ),
       padding: EdgeInsets.all(20),
-      child: GridView.count(
-        crossAxisCount: 3,
-        crossAxisSpacing: 15,
-        mainAxisSpacing: 15,
-        children: gridItems,
-      ),
+      child: body,
     );
   }
 }
