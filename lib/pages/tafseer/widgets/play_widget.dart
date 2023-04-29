71f861ec5f1d48b0b8c164e1a838b9a2fc4ff043
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2023-04-29 14:16:33 +0300

    Up and running

--- lib/pages/tafseer/widgets/play_widget.dart
+++ lib/pages/tafseer/widgets/play_widget.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:simply/simply.dart';
+import 'package:yatadabaron/_modules/service_contracts.module.dart';
 
 class VersePlayWidget extends StatefulWidget {
   final int verseId;
@@ -21,54 +23,16 @@ class _VersePlayWidgetState extends State<VersePlayWidget> {
   StreamSubscription<AudioPlayerState>? streamSubscription;
   AudioPlayerState state = AudioPlayerState.initial;
   String reciterKey = "Husary_128kbps";
-  final reciterNameMap = {
-    "AbdulSamad_64kbps_QuranExplorer.Com": "Abdul Basit Abdul Samad",
-    "Abdul_Basit_Mujawwad_128kbps": "Abdul Basit Mujawwad",
-    "Abdul_Basit_Murattal_192kbps": "Abdul Basit Murattal",
-    "Abdullaah_3awwaad_Al-Juhaynee_128kbps": "Abdullaah Awaad Al-Juhaynee",
-    "Abdullah_Basfar_192kbps": "Abdullah Basfar",
-    "Abdullah_Matroud_128kbps": "Abdullah Matroud",
-    "Abdurrahmaan_As-Sudais_192kbps": "Abdurrahmaan As-Sudais",
-    "Abu_Bakr_Ash-Shaatree_128kbps": "Abu Bakr Ash-Shaatree",
-    "Ahmed_Neana_128kbps": "Ahmed Neana",
-    "Ahmed_ibn_Ali_al-Ajamy_128kbps_ketaballah.net": "Ahmed Ibn Ali Al-Ajamy",
-    "Akram_AlAlaqimy_128kbps": "Akram AlAlaqimy",
-    "Alafasy_128kbps": "Alafasy",
-    "Ali_Hajjaj_AlSuesy_128kbps": "Ali Hajjaj AlSuesy",
-    "Ali_Jaber_64kbps": "Ali Jaber",
-    "Ayman_Sowaid_64kbps": "Ayman Sowaid",
-    "Fares_Abbad_64kbps": "Fares Abbad",
-    "Ghamadi_40kbps": "Ghamadi",
-    "Hani_Rifai_192kbps": "Hani Rifai",
-    "Hudhaify_128kbps": "Hudhaify",
-    "Husary_128kbps": "Al Husary",
-    "Husary_128kbps_Mujawwad": "Al Husary - Mujawwad",
-    "Husary_Muallim_128kbps": "Al Husary - Muallim",
-    "Khaalid_Abdullaah_al-Qahtaanee_192kbps": "Khaalid Abdullaah Al-Qahtaanee",
-    "MaherAlMuaiqly128kbps": "Maher AlMuaiqly",
-    "Minshawy_Mujawwad_192kbps": "Al Minshawy - Mujawwad",
-    "Minshawy_Murattal_128kbps": "Al Minshawy - Murattal",
-    "Mohammad_al_Tablaway_128kbps": "Mohammad Al Tablaway",
-    "Muhammad_AbdulKareem_128kbps": "Muhammad AbdulKareem",
-    "Muhammad_Ayyoub_128kbps": "Muhammad Ayyoub",
-    "Muhammad_Jibreel_128kbps": "Muhammad Jibreel",
-    "Muhsin_Al_Qasim_192kbps": "Muhsin Al Qasim",
-    "Nasser_Alqatami_128kbps": "Nasser Alqatami ",
-    "Sahl_Yassin_128kbps": "Sahl Yassin ",
-    "Salaah_AbdulRahman_Bukhatir_128kbps": "Salaah AbdulRahman Bukhatir",
-    "Salah_Al_Budair_128kbps": "Salah Al Budair",
-    "Saood_ash-Shuraym_128kbps": "Saood Ash-Shuraym",
-    "Yaser_Salamah_128kbps": "Yaser Salamah",
-    "Yasser_Ad-Dussary_128kbps": "Yasser Ad-Dussary",
-    "ahmed_ibn_ali_al_ajamy_128kbps": "Ahmed Ibn Ali Al-Ajamy",
-    "aziz_alili_128kbps": "Aziz Alili",
-    "khalefa_al_tunaiji_64kbps": "Khalefa Al Tunaiji",
-    "mahmoud_ali_al_banna_32kbps": "Mahmoud Ali Al Banna",
-  };
+  final appSettingsService = Simply.get<IAppSettingsService>();
 
   @override
   void initState() {
     super.initState();
+    if (appSettingsService.currentValue.reciterKey != null) {
+      setState(() {
+        reciterKey = appSettingsService.currentValue.reciterKey!;
+      });
+    }
     streamSubscription = player.playerStateStream.map((originalEvent) {
       if (originalEvent.processingState == ProcessingState.completed) {
         return AudioPlayerState.completed;
@@ -137,12 +101,13 @@ class _VersePlayWidgetState extends State<VersePlayWidget> {
                 .toList(),
             onChanged: state == AudioPlayerState.initial ||
                     state == AudioPlayerState.completed
-                ? (v) {
-                    setState(() {
-                      if (v != null) {
+                ? (v) async {
+                    if (v != null) {
+                      await appSettingsService.updateReciter(v);
+                      setState(() {
                         reciterKey = v;
-                      }
-                    });
+                      });
+                    }
                   }
                 : null,
             value: reciterKey,
@@ -161,3 +126,48 @@ class _VersePlayWidgetState extends State<VersePlayWidget> {
 }
 
 enum AudioPlayerState { paused, initial, playing, completed }
+
+final reciterNameMap = {
+  "AbdulSamad_64kbps_QuranExplorer.Com": "Abdul Basit Abdul Samad",
+  "Abdul_Basit_Mujawwad_128kbps": "Abdul Basit Mujawwad",
+  "Abdul_Basit_Murattal_192kbps": "Abdul Basit Murattal",
+  "Abdullaah_3awwaad_Al-Juhaynee_128kbps": "Abdullaah Awaad Al-Juhaynee",
+  "Abdullah_Basfar_192kbps": "Abdullah Basfar",
+  "Abdullah_Matroud_128kbps": "Abdullah Matroud",
+  "Abdurrahmaan_As-Sudais_192kbps": "Abdurrahmaan As-Sudais",
+  "Abu_Bakr_Ash-Shaatree_128kbps": "Abu Bakr Ash-Shaatree",
+  "Ahmed_Neana_128kbps": "Ahmed Neana",
+  "Ahmed_ibn_Ali_al-Ajamy_128kbps_ketaballah.net": "Ahmed Ibn Ali Al-Ajamy",
+  "ahmed_ibn_ali_al_ajamy_128kbps": "Ahmed Ibn Ali Al-Ajamy",
+  "Akram_AlAlaqimy_128kbps": "Akram AlAlaqimy",
+  "Alafasy_128kbps": "Alafasy",
+  "Ali_Hajjaj_AlSuesy_128kbps": "Ali Hajjaj AlSuesy",
+  "Ali_Jaber_64kbps": "Ali Jaber",
+  "Ayman_Sowaid_64kbps": "Ayman Sowaid",
+  "aziz_alili_128kbps": "Aziz Alili",
+  "Fares_Abbad_64kbps": "Fares Abbad",
+  "Ghamadi_40kbps": "Ghamadi",
+  "Hani_Rifai_192kbps": "Hani Rifai",
+  "Hudhaify_128kbps": "Hudhaify",
+  "Husary_128kbps": "Al Husary",
+  "Husary_128kbps_Mujawwad": "Al Husary - Mujawwad",
+  "Husary_Muallim_128kbps": "Al Husary - Muallim",
+  "Khaalid_Abdullaah_al-Qahtaanee_192kbps": "Khaalid Abdullaah Al-Qahtaanee",
+  "khalefa_al_tunaiji_64kbps": "Khalefa Al Tunaiji",
+  "MaherAlMuaiqly128kbps": "Maher AlMuaiqly",
+  "mahmoud_ali_al_banna_32kbps": "Mahmoud Ali Al Banna",
+  "Minshawy_Mujawwad_192kbps": "Al Minshawy - Mujawwad",
+  "Minshawy_Murattal_128kbps": "Al Minshawy - Murattal",
+  "Mohammad_al_Tablaway_128kbps": "Mohammad Al Tablaway",
+  "Muhammad_AbdulKareem_128kbps": "Muhammad AbdulKareem",
+  "Muhammad_Ayyoub_128kbps": "Muhammad Ayyoub",
+  "Muhammad_Jibreel_128kbps": "Muhammad Jibreel",
+  "Muhsin_Al_Qasim_192kbps": "Muhsin Al Qasim",
+  "Nasser_Alqatami_128kbps": "Nasser Alqatami ",
+  "Sahl_Yassin_128kbps": "Sahl Yassin ",
+  "Salaah_AbdulRahman_Bukhatir_128kbps": "Salaah AbdulRahman Bukhatir",
+  "Salah_Al_Budair_128kbps": "Salah Al Budair",
+  "Saood_ash-Shuraym_128kbps": "Saood Ash-Shuraym",
+  "Yaser_Salamah_128kbps": "Yaser Salamah",
+  "Yasser_Ad-Dussary_128kbps": "Yasser Ad-Dussary",
+};
--- lib/services/app_settings_service.dart
+++ lib/services/app_settings_service.dart
@@ -3,7 +3,6 @@ import 'package:yatadabaron/_modules/service_contracts.module.dart';
 import 'package:yatadabaron/commons/constants.dart';
 import 'package:yatadabaron/_modules/models.module.dart';
 
-//Imp
 class AppSettingsService implements IAppSettingsService {
   AppSettingsService({
     required this.sharedPreferences,
@@ -16,6 +15,7 @@ class AppSettingsService implements IAppSettingsService {
     return AppSettings(
       language: "ar",
       nightMode: _getBool(Constants.PREF_NIGHT_MODE_KEY) ?? false,
+      reciterKey: _getString(Constants.PREF_RECITER_KEY),
     );
   }
 
@@ -27,11 +27,24 @@ class AppSettingsService implements IAppSettingsService {
         );
   }
 
+  String? _getString(String k) {
+    try {
+      return this.sharedPreferences.getString(k);
+    } catch (e) {
+      return null;
+    }
+  }
+
   bool? _getBool(String k) {
     try {
       return this.sharedPreferences.getBool(k);
     } catch (e) {
       return null;
     }
+  }
+
+  @override
+  Future<void> updateReciter(String reciterKey) async {
+    await sharedPreferences.setString(Constants.PREF_RECITER_KEY, reciterKey);
   }
 }
