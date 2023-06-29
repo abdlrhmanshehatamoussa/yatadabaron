427807e65d99c69a1bb29aa07adc5284d0cec024
abdlrhman shehata <abdlrhmanshehata@gmail.com>
2023-06-30 00:44:34 +0300

    Revert "Upgrading packages"
    This reverts commit d994cb87389a3147f26ff1d1fe14250a7c16e780.

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:connectivity_plus_web/connectivity_plus_web.dart';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_app_check_web/firebase_app_check_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
@@ -17,6 +18,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FirebaseFirestoreWeb.registerWith(registrar);
+  ConnectivityPlusPlugin.registerWith(registrar);
   FirebaseAnalyticsWeb.registerWith(registrar);
   FirebaseAppCheckWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
